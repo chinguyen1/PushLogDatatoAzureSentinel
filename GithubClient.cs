f2c760694cfd1ecda413c2fc550e2fa29dfa58f6
Chi <nguyen.chi.byu@gmail.com>
2020-04-28 22:35:51 -0700

    Initial Commit

---
+++ GithubClient.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace PushSampleDataToSentinelLogs
+{
+    public class Github
+    {
+        static readonly string access_token = "21de750f22a2a6283248d21380173074ca8ec575";
+        public Github()
+        {
+            
+        }
+
+        //Get all files from a repo
+        public async Task<Directory> getRepo(string owner, string name)
+        {
+            HttpClient client = new HttpClient();
+            Directory root = await readDirectory("root", client, String.Format("https://api.github.com/repos/{0}/{1}/contents/", owner, name));
+            client.Dispose();
+            return root;
+        }
+
+        private async Task<String> GetDirectory(String name, HttpClient client, string uri)
+        {
+            //get the directory contents
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri);
+            request.Headers.Add("Authorization",
+                "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(String.Format("{0}:{1}", access_token, "x-oauth-basic"))));
+            request.Headers.Add("User-Agent", "lk-github-client");
+
+            //parse result
+            HttpResponseMessage response = await client.SendAsync(request);
+            String jsonStr = await response.Content.ReadAsStringAsync(); ;
+            response.Dispose();
+            return jsonStr;
+            
+        }
+
+        private async Task<Directory> readDirectory(String name, HttpClient client, string uri)
+        {
+            var jsonStr = await GetDirectory(name, client, uri);
+
+            //read in data
+            FileInfo[] dirContents = JsonConvert.DeserializeObject<FileInfo[]>(jsonStr);
+
+            var rootDirectoryName = "Sample Data";
+            Directory result;
+            result.name = rootDirectoryName;
+            result.subDirs = new List<Directory>();
+            result.files = new List<FileData>();
+            foreach (FileInfo dir in dirContents)
+            {
+                if(dir.name == rootDirectoryName)
+                {
+                    if (dir.type == "dir")
+                    {
+                        var currentDirectory = System.IO.Directory.GetCurrentDirectory();
+                        var basePath = currentDirectory.Split(new string[] { "\\bin" }, StringSplitOptions.None)[0];
+                        var dirPath = basePath + "\\" + rootDirectoryName;
+
+                        if (!System.IO.Directory.Exists(dirPath))
+                        {
+                            System.IO.Directory.CreateDirectory(dirPath);
+                        }
+
+                        await ProcessFiles(dir.name, client, dir._links.self, dirPath, result);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private async Task<Directory> ProcessFiles(String directoryName, HttpClient client, string uri, string dirPath, Directory directory)
+        {
+            var jsonStr = await GetDirectory(directoryName, client, uri);
+            FileInfo[] dirContents = JsonConvert.DeserializeObject<FileInfo[]>(jsonStr);
+
+            if (!System.IO.Directory.Exists(dirPath))
+            {
+                System.IO.Directory.CreateDirectory(dirPath);
+            }
+
+            foreach (FileInfo file in dirContents)
+            {
+                var filePath = dirPath + "\\" + file.name;
+
+                if (file.type == "dir")
+                {
+                    Directory subDir;
+                    subDir.name = file.name;
+                    subDir.subDirs = new List<Directory>();
+                    subDir.files = new List<FileData>();
+
+                    await ProcessFiles(file.name, client, file._links.self, filePath, subDir);
+                    directory.subDirs.Add(subDir);
+                }
+                else
+                {
+                    FileData data;
+                    data.name = file.name;
+                    var content = await GetDirectory(file.name, client, file._links.self);
+                    data.hasFileBeenModified = false;
+                    data.isNewFile = false;
+
+                    if (file.name.Contains(".csv"))
+                    {
+                        var fileName = file.name.Replace(".csv", ".json");
+                        filePath = dirPath + "\\" + fileName;
+                        data.name = fileName;
+                        data.contents = JsonConvert.SerializeObject(content);
+                    }
+                    else
+                    {
+                        data.contents = content;
+                    }
+
+                    var response = AddFiles(filePath, data);
+                    //data.hasFileBeenModified = response.;
+                    directory.files.Add(response);
+                }
+            }
+
+            return directory;
+        }
+
+        private FileData AddFiles(String fileName, FileData file)
+        {
+            if (File.Exists(fileName))
+            {
+                file.hasFileBeenModified = CheckIfFileModifiedSinceLastDownload(fileName, file);
+                //if (fileName.Contains(".csv"))
+                //{
+                //    //var json = JsonConvert.SerializeObject(file.contents);
+                //    //fileName = fileName.Replace(".csv", ".json");
+                    File.WriteAllText(fileName, file.contents);
+                //}
+            }
+            else
+            {
+                file.isNewFile = true;
+                //var json = JsonConvert.SerializeObject(file.contents);
+                //fileName = fileName.Replace(".csv", ".json");
+                File.WriteAllLines(fileName, new string[] { file.contents });
+            }
+
+            return file;
+        }
+
+        private bool CheckIfFileModifiedSinceLastDownload(String filePath, FileData file)
+        {
+            var existingFileContent = File.ReadAllText(filePath).Trim();
+            if (existingFileContent != file.contents)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+
+
+    //JSON parsing methods
+    struct LinkFields
+    {
+        public String self;
+    }
+    struct FileInfo
+    {
+        public String name;
+        public String type;
+        public String download_url;
+        public LinkFields _links;
+    }
+
+    //Structs used to hold file data
+    public struct FileData
+    {
+        public string name;
+        public String contents;
+        public Boolean hasFileBeenModified;
+        public Boolean isNewFile;
+    }
+    public struct Directory
+    {
+        public String name;
+        public List<Directory> subDirs;
+        public List<FileData> files;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushSampleDataToSentinelLogs
+{
+    class ApiExample
+    { 
+        // Update customerId to your Log Analytics workspace ID
+        static string customerId = "";
+
+        // For sharedKey, use either the primary or the secondary Connected Sources client authentication key   
+        static string sharedKey = "";
+
+        // LogName is name of the event type that is being submitted to Azure Monitor
+        static string LogName = "ForcepointDLP_sample";
+
+        // You can use an optional field to specify the timestamp from the data. If the time field is not specified, Azure Monitor assumes the time is the message ingestion time
+        static string TimeStampField = "";
+
+        static void Main()
+        {
+            var githubClient = new Github();
+            var task = githubClient.getRepo("Azure", "Azure-Sentinel");
+            task.Wait();
+            var dir = task.Result;
+
+            Console.WriteLine("Repo: " + dir.name);
+            WriteRepoToConsole(dir);
+            Console.WriteLine("Are you sure to add (Yes(Y)/No(N))");
+            var test = Console.ReadLine();
+
+
+            //if (test.ToUpper()=="Y" || test.ToUpper() == "YES")
+            //{
+            //    // do whhe
+            //}
+
+            // Create a hash for the API signature
+            var datestring = DateTime.UtcNow.ToString("r");
+            string json = ReadFile();
+            var jsonBytes = Encoding.UTF8.GetBytes(json);
+            string stringToHash = "POST\n" + jsonBytes.Length + "\napplication/json\n" + "x-ms-date:" + datestring + "\n/api/logs";
+            string hashedString = BuildSignature(stringToHash, sharedKey);
+            string signature = "SharedKey " + customerId + ":" + hashedString;
+
+            PostData(signature, datestring, json);
+        }
+
+        private static void WriteRepoToConsole(Directory directory)
+        {
+            foreach (var file in directory.files)
+            {
+                if (file.hasFileBeenModified)
+                {
+                    Console.WriteLine("  **" + file.name);
+                }
+                else if (file.isNewFile)
+                {
+                    Console.WriteLine("  ++" + file.name);
+                }
+                else
+                {
+                    Console.WriteLine("  " + file.name);
+                }
+            }
+
+            foreach (var file in directory.subDirs)
+            {
+                Console.WriteLine("Sub Dir: " + file.name);
+                WriteRepoToConsole(file);
+            }
+        }
+
+        // Build the API signature
+        public static string BuildSignature(string message, string secret)
+        {
+            var encoding = new System.Text.ASCIIEncoding();
+            byte[] keyByte = Convert.FromBase64String(secret);
+            byte[] messageBytes = encoding.GetBytes(message);
+            using (var hmacsha256 = new HMACSHA256(keyByte))
+            {
+                byte[] hash = hmacsha256.ComputeHash(messageBytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        public static string ReadFile()
+        {
+            // An example JSON object, with key/value pairs
+            //string json = @"[{""TenantId"":""1063531e-68b1-4ff2-9546-d8d2d8a584c4"",""SourceSystem"":""OpsManager""},{""TimeGenerated"":""2020-02-06T09:26:24.94Z"",""ReceiptTime
+            //                "":""1580980000000""}]";
+            try
+            {
+                var filePath = Environment.CurrentDirectory + @"\SampleData\CustomLog\";
+                using (StreamReader streamReader = new StreamReader(filePath))
+                {
+                    var json = streamReader.ReadToEnd();
+                    return json;
+                }
+            }
+            catch (Exception excep)
+            {
+                Console.WriteLine("Read File exception: " + excep.Message);
+                return "Read File error";
+            }
+
+        }
+
+        // Send a request to the POST API endpoint
+        public static void PostData(string signature, string date, string json)
+        {
+            try
+            {
+                string url = "https://" + customerId + ".ods.opinsights.azure.com/api/logs?api-version=2016-04-01";
+
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                client.DefaultRequestHeaders.Add("Log-Type", LogName);
+                client.DefaultRequestHeaders.Add("Authorization", signature);
+                client.DefaultRequestHeaders.Add("x-ms-date", date);
+                client.DefaultRequestHeaders.Add("time-generated-field", TimeStampField);
+
+                HttpContent httpContent = new StringContent(json, Encoding.UTF8);
+                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                Task<HttpResponseMessage> response = client.PostAsync(new Uri(url), httpContent);
+
+                HttpContent responseContent = response.Result.Content;
+                string result = responseContent.ReadAsStringAsync().Result;
+                Console.WriteLine("Data successfully posted: " + result);
+            }
+            catch (Exception excep)
+            {
+                Console.WriteLine("API Post Exception: " + excep.Message);
+            }
+        }
+    }
+}
